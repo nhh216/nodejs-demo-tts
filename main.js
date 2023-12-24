24763dba61a5f3fd6a1c156445fd1c936abb8084
hungnh <nhh.216@gmail.com>
2023-12-24 22:58:07 +0700

    update

--- main.js
+++ main.js
@@ -13,35 +13,35 @@ app.use(cors({origin: '*', allowedHeaders: '*'}))
 router.get('/test-tts', function(req, res, next) {
   const text = req.query.text;
   console.log(text)
-  fs.writeFile("//home/hungnh/Desktop/projects/vietTTS/assets/transcript.txt", `${text}`, function(err) {
+  fs.writeFile("/root/tts-demo/clone-tts/assets/transcript.txt", `${text}`, function(err) {
     if(err) {
       return console.log(err);
     }
-    console.log("The file was saved!");
   });
-  const FILE_NAME = `assets/infore/audio_${Date.now()}.wav`
-  exec(`cd /home/hungnh/Desktop/projects/vietTTS && FILE_NAME='${FILE_NAME}' /home/hungnh/Desktop/projects/vietTTS/scripts/quick_start.sh`,
+  const fileName = `audio_${Date.now()}.wav`;
+  const FILE_NAME = `assets/infore/${fileName}`
+  exec(`FILE_NAME='${FILE_NAME}' /root/tts-demo/clone-tts/scripts/quick_start.sh`,
     (error, stdout, stderr) => {
       console.log(stdout);
       console.log(stderr);
       if (error !== null) {
         next({ data: "Error", status: 500})
       }
-      var filePath = `/home/hungnh/Desktop/projects/vietTTS/${FILE_NAME}`; // Or format the path using the `id` rest param
-      var fileName = "report.wav"; // The default name the browser will use
-      var mimetype = mime.getType(filePath);
-      res.setHeader('Content-disposition', 'attachment; filename=' + FILE_NAME);
-      res.setHeader('Content-type', mimetype);
-      res.download(filePath, fileName);
+      // var filePath = `/home/hungnh/Desktop/projects/vietTTS/${FILE_NAME}`; // Or format the path using the `id` rest param
+      // var fileName = "report.wav"; // The default name the browser will use
+      // var mimetype = mime.getType(filePath);
+      // res.setHeader('Content-disposition', 'attachment; filename=' + FILE_NAME);
+      // res.setHeader('Content-type', mimetype);
+      // res.download(filePath, fileName);
       // var filestream = fs.createReadStream(filePath);
       // filestream.pipe(res);
-      // next({ data: null, status: 200})
+      next({ data: fileName, status: 200})
     });
 });
 
 router.get('/listen', function(req, res, next) {
   var filePath = `/home/hungnh/Desktop/projects/vietTTS/assets/infore/audio_1703405924612.wav`; // Or format the path using the `id` rest param
-  var fileName = "audio_1703405924612.wav"; // The default name the browser will use
+  var fileName = req.query.fileName; // The default name the browser will use
   var mimetype = mime.getType(filePath);
   res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
   res.setHeader('Content-type', mimetype);
